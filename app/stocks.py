00e9c05d76ef9f229009976ed2bfb0401165562f
james-shi-ys866 <ys866@georgetown.edu>
2024-11-13 18:08:29 -0500

    refactored env var

--- app/stocks.py
+++ app/stocks.py
@@ -2,16 +2,13 @@
 
 # LOCAL DEV (ENV VARS)
 
-import os
+from statistics import mean
 
-from dotenv import load_dotenv
-from pandas import read_csv
+import requests
 from plotly.express import line
 
 
-load_dotenv() # looks in the ".env" file for env vars
-
-API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
+from app.alpha_service import API_KEY
 
 
 def fetch_stocks_csv(symbol):

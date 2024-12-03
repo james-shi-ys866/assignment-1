e651f63eabb9052f1b56fdd3df2c2fb52ec4b62b
james-shi-ys866 <ys866@georgetown.edu>
2024-12-03 18:15:34 -0500

    test_1 cocktail

--- web_app/routes/cocktail_routes.py
+++ web_app/routes/cocktail_routes.py
@@ -1,33 +1,28 @@
 # this is the "web_app/routes/home_routes.py" file...
 
-from flask import Blueprint, request, render_template
+from flask import Blueprint, requests, render_template
 
-product_routes = Blueprint("product_routes", __name__)
+cocktail_routes = Blueprint("cocktail_routes", __name__)
 
-@product_routes.route("/products")
-def products_list():
-    print("PRODUCTS...")
+@cocktail_routes.route("/cocktails")
+def cocktails_list():
+    print("COCKTAILS...")
 
+    request_url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
+    response = requests.get(request_url)
+    
 
-    products = [
-        {
-            'name': 'Strawberries',
-            'description': 'Juicy organic strawberries.',
-            'price': 4.99,
-            'url': 'https://picsum.photos/id/1080/360/200'
-        },
-        {
-            'name': 'Cup of Tea',
-            'description': 'An individually-prepared tea or coffee of choice.',
-            'price': 3.49,
-            'url': 'https://picsum.photos/id/225/360/200'
-        },
-        {
-            'name': 'Textbook',
-            'description': 'It has all the answers.',
-            'price': 129.99,
-            'url': 'https://picsum.photos/id/24/360/200'
+    data = response.json()
+    
+
+    cocktails = []
+    for drink in data["drinks"][:20]:
+        cocktail = {
+            "name": drink["strDrink"],
+            "image": drink["strDrinkThumb"],
+            "id": drink["idDrink"]
         }
-    ]
+        cocktails.append(cocktail)
 
-    return render_template("products.html", products=products)
+
+    return render_template("cocktails.html", cocktails=cocktails)
